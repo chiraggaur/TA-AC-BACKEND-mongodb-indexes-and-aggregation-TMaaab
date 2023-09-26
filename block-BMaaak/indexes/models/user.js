2e454506d08998dd7bf8042da02b7061579deb38
chirag gaur <chiragwalker@gmail.com>
2023-09-26 21:56:10 +0530

    done

--- block-BMaaak/indexes/models/user.js
+++ block-BMaaak/indexes/models/user.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 
 // schema
 var userSchema = new Schema({
-  title: { type: String },
+  name: { type: String },
   username: { type: String, unique: true },
   email: { type: String, unique: true },
   address: {
@@ -14,5 +14,10 @@ var userSchema = new Schema({
     pin: { type: Number },
   },
 });
+
+// 1. Define unique indexes on username and email. // added above inside schema
+//2. define compound indexes on state and country field inside address document. Each country must have states which are unique.
+userSchema.index({ "address.state": 1, "address.city": 1 }); // order matters state come first
+// exporting the module
 
 module.exports = mongoose.model("User", userSchema); // collection
